2bf5da5cf3a07393e810e171154540a5d50afddb
Thilankavishka <thilankavishka25@gmail.com>
2025-10-03 15:09:21 +0530

    Add initial implementation of a simple addition program in python

---
+++ python_basics/first.py
@@ -0,0 +1,10 @@
+x = int(input("Enter your First Number:"))
+y = int(input("Enter your Second Number:"))
+sum = x + y
+print("Your Sum is:", sum)
+
+
+def add(a, b):
+    return a + b
+
+print(add(10,58))
