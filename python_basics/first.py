43e4448d8a7acf352163c218c2293fd2c2276abd
Thilankavishka <thilankavishka25@gmail.com>
2025-10-03 15:11:50 +0530

    Refactor addition program to improve structure and add new functionality

--- python_basics/first.py
+++ python_basics/first.py
@@ -3,8 +3,13 @@ y = int(input("Enter your Second Number:"))
 sum = x + y
 print("Your Sum is:", sum)
 
-
+#function
 def add(a, b):
     return a + b
 
 print(add(10,58))
+
+#for loop
+n = 25
+for i in range(n):
+    print(i)
