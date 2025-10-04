473284cbc3eadfbd8a245f8dabbf533489685dcd
Thilankavishka <thilankavishka25@gmail.com>
2025-10-04 08:21:19 +0530

    Add initial implementation of perimeter and area calculation program

---
+++ python_basics/Activity_02.py
@@ -0,0 +1,7 @@
+len = int(input("Enter length: "))
+wid = int(input("Enter width: "))
+
+perimeter = 2 * (len + wid)
+area = len * wid
+print("Perimeter is: ", perimeter)
+print("Area is: ", area)
---
+++ python_basics/second.py
@@ -0,0 +1,33 @@
+n1 = 10
+st = "Thilan"
+n3 = 70.5
+n4 = True
+print(type(n1))
+print(type(st))
+print(type(n3))
+print(type(n4))
+
+n5 = n1
+print(n1)
+print(type(n5))
+
+abc = 100
+Abc = 45
+ABC = 78
+
+print(abc)
+print(Abc)
+print(ABC)
+
+variable_1 = 5
+name_of_student = "Thilan"
+nameOfStudent = "Thilan"
+
+num1 = float(input("Enter Num 1: "))
+num2 = float(input("Enter Num 2: "))
+sum = num1 + num2
+print(sum)
+
+
+print("Max num is: ",max(10, 20,30,44,5,22,8))
+print("Min is:",min(10, 20,30,44,5,22,8))
